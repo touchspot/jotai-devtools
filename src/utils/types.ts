03144b44a24f45c9e7835aebcd7719a3532bed0f
('3') <mozisan@users.noreply.github.com>
2025-03-27 00:11:53 +0900

    chore: remove files

--- src/utils/types.ts
+++
@@ -1,7 +0,0 @@
-// FIXME https://github.com/reduxjs/redux-devtools/issues/1097
-// This is an INTERNAL type alias.
-export type Message = {
-  type: string;
-  payload?: any;
-  state?: any;
-};
--- types/redux.d.ts
+++
@@ -1,1 +0,0 @@
-import '@redux-devtools/extension';
